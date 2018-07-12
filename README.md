f64ec29f06295315bb93621b45c25d632399f837
Joel Zeng <joelzeng@hotmail.com>
2018-07-12 17:51:17 -0400

    checkpoint

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CustomizationBackup OSX
 Shell scripts helping install setup.
+
+## Usage
+git clone repo...
+chmod u+x executeAll.sh
+chmod u+X updateAll.sh
+
+## executeAll.sh
+Executes every installation and customization script.
+
+## updateAll.sh
+update packages that are commonly needed to update. Run every month or so to ensure optimal system performance.
+
+
+
+
---
+++ executeAll.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Author: Joel zeng, <joelzeng@hotmail.com>
+
+APPLICATION_DIR='~/Applications'
+
+echo '**************************************************'
+echo '*******Executing package script installation******'
+echo '**************************************************'
+
+# System package installation
+echo '########## System Package Installation ###########'
+
+echo '***** Homebrew *****'
+chmod u+x ./homebrew/homebrewinstall.sh
+./homebrew/homebrewinstall.sh
+
+# modiftying iterm and terminal
+echo '***** Terminal *****'
+
+for scripts in ./terminal/*.sh; do
+  echo $scripts
+done
